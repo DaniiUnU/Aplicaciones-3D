7afff0c8f494eca404ff2652c50de48a3ab2c76c
Daniela_Aponte <90203432+DaniiUnU@users.noreply.github.com>
2021-09-06 22:56:14 -0500

    Eventos de mouse y teclado

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -9,18 +9,121 @@ function start() {
             nlat: 30,
             nlong: 30,
             radius: 3,
-            //textures: 'https://lizgar.github.io/A3D/js/earth.jpg'
+            // textures: 'https://lizgar.github.io/A3D/js/earth.jpg'
         }
     );
 
 
     PhiloGL('glcanvas',
         {
-            camera: {
-                position: {
-                    x: 0, y: 0, z: -10
+            events: {
+                onDragStart: function (e) {
+                    cameraControl.onDragStart(e);
+                },
+                onDragMove: function (e) {
+                    cameraControl.onDragMove(e);
+                },
+                onKeyDown: function (e) {
+                    objectControl.onKeyDown(e);
                 }
             },
+            onLoad: function (app) {
+                var camera = app.camera;
+                camera.fov = 37;
+                camera.update();
+                cameraControl = new CameraControl(app.camera);
+            },
+
+            camera: {
+                position: {
+                    x: 0, y: 0, z: -13
+                }
+            },
+
+            events: {
+                onKeyDown: function (e) {
+                    switch (e.key) {
+                        case 'up':
+                            tierra.position.y += 2;
+                            break;
+                        case 'down':
+                            tierra.position.y -= 2;
+                            break;
+                        case 'left':
+                            tierra.position.x += 2;
+                            break;
+                        case 'right':
+                            tierra.position.x -= 2;
+                            break;
+                    }
+                    tierra.update();
+                },
+
+                onTouchStart: function (e) {
+                    e.stop();
+                    this.pos = {
+                        x: e.x,
+                        y: e.y
+                    };
+                    this.dragging = true;
+                },
+                onTouchCancel: function () {
+                    this.dragging = false;
+                },
+                onTouchEnd: function () {
+                    this.dragging = false;
+                    theta = this.scene.models[0].rotation.y;
+                },
+                onTouchMove: function (e) {
+                    e.stop();
+                    var z = this.camera.position.z,
+                        sign = Math.abs(z) / z,
+                        pos = this.pos;
+
+                    this.scene.models.forEach(function (m) {
+                        m.rotation.y += -(pos.x - e.x) / 100;
+                        m.update();
+                    });
+
+                    pos.x = e.x;
+                    pos.y = e.y;
+                },
+                onDragStart: function (e) {
+                    this.pos = {
+                        x: e.x,
+                        y: e.y
+                    };
+                    this.dragging = true;
+                },
+                onDragCancel: function () {
+                    this.dragging = false;
+                },
+                onDragEnd: function () {
+                    this.dragging = false;
+                    theta = this.scene.models[0].rotation.y;
+                },
+                onDragMove: function (e) {
+                    var z = this.camera.position.z,
+                        sign = Math.abs(z) / z,
+                        pos = this.pos;
+
+                    this.scene.models.forEach(function (m) {
+                        m.rotation.y += -(pos.x - e.x) / 100;
+                        m.update();
+                    });
+
+                    pos.x = e.x;
+                    pos.y = e.y;
+                },
+                onMouseWheel: function (e) {
+                    e.stop();
+                    var camera = this.camera;
+                    camera.position.z += e.wheel;
+                    camera.update();
+                }
+
+            },
+
 
             /*textures: {
 
@@ -76,4 +179,5 @@ function start() {
 
 
         });
+
 }
